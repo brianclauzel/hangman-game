1702a2bd384cfb1aa231eb4ae1a9f075e68ec8de
brianclauzel <brianthomasclauzel@gmail.com>
2018-02-16 22:50:38 -0800

    yo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,101 @@
 // global variables
 
-var guess = [];
+var countries = [];   
+countries[0] = {name: "new zealand", hint: "Lots of sheep!"};
 
-var wrongGuess = [];
-var word = ['yep','nope'];
-var randomWord;
-var underScores = [];
+countries[1] = {name: "russia", hint: "hand me some vodka!"};
 
-var wordBlanks;
+countries[2] = {name: "united states of america", hint: "tide pods are on every menu!"};
 
-var guessesLeft = [10];
-var winCounter;
-var lossCounter;
+countries[3] = {name: "saudi arabia", hint: "land of oil and headwear"};
+
+countries[4] = {name: "germany", hint: "dont be a braut, your the werst (lol?)"};
+
+countries[5] = {name: "zimbabwe", hint: "no hint, good luck!"};
+
+var countryName;
+var lettersGuessed;
+var randomCountry;
+var pickedCountry;
+var countryLength
+var countrySubstring
+var placeholder;
+var wins = 0;
+var losses = 0;
+var guessesLeft;
+var hint;
+var key;
 
 
-function startGame() {
+function newGame() {
+    placeholder = "";
+    guessesLeft = 8;
+    lettersGuessed = [];
+    randomCountry = Math.floor(Math.random() * countries.length);
+    pickedCountry = countries.splice(randomCountry, 1);
+    countryName = pickedCountry[0].name;
+    hint = pickedCountry[0].hint;
+    countryLength = countryName.length;
+    countrySubstring = countryName.substring;
 
-    randomWord = word[Math.floor(Math.random() * word.length)];
+    for (var i = 0; i < countryName.length; i++) {
 
-    for (var i = 0; i < randomWord.length; i++) {
-        
-        underScores.push('_');
+    if (countryName.substring(i, i + 1) === " ") {
 
-    }
-
-    document.getElementById('word-blanks').innerHTML = underScores.join(" ");
-
-    wrongGuess = [];
-
-    guessesLeft = [10];
-
-    document.getElementById('guesses-left').innerHTML = guessesLeft;
-
-}
-
-function
-
-document.onkeyup = function(event) {
-
-    guess = event.key;
-
-    if (randomWord.indexOf(guess) > -1) {
-
-        for (var i = 0; i < randomWord.length; i++) {
-            
-            if (randomWord[i] === guess) {
-
-                underScores[i] === guess;
-
-            }
-
+            placeholder = placeholder + " ";
         }
-        
-    }
-
+    
     else {
+            placeholder = placeholder + "_";
+        }
 
-        wrongGuess.push(guess);
+ }
 
-        guessesLeft--;
-        
-        document.getElementById('guesses-left').innerHTML = guessesLeft;
-
-
-    }
-
+    document.querySelector("#win-counter").innerHTML = wins;
+    document.querySelector("#loss-counter").innerHTML = losses;
+    document.querySelector("#word-blanks").innerHTML = placeholder;
+    document.querySelector("#wrong-guesses").innerHTML = lettersGuessed;
+    document.querySelector("#guesses-left").innerHTML = guessesLeft;
+    
 }
 
-startGame();
+    newGame();
+
+    document.onkeyup = function(event) {
+        key = event.key;
+
+        var correct = 0;
+        //checking for correct guesses when clicked
+        for (var i = 0; i < countryLength; i++) {
+           if (key == countryName.substring(i, i + 1)) {
+                correct++;
+                placeholder = placeholder.substring(0, 1) + key + placeholder.substring(i + 1, placeholder.length + 1);
+                document.querySelector("#word-blanks").innerHTML = placeholder;
+           }
+        } 
+
+    //wrong guesses
+
+        if (correct == 0) {
+            guessesLeft--;
+            lettersGuessed.push(key);
+            document.querySelector("#guesses-left").innerHTML = guessesLeft;
+            document.querySelector("#wrong-guesses").innerHTML = lettersGuessed;
+        }
+    //if you win
+
+        if (placeholder == countryName) {
+            wins++;
+            alert("you big winner! The answer is " + countryName);
+            document.querySelector("#win-counter").innerHTML = wins;
+        }
+    //if you lose
+        if (guessesLeft == 0) {
+            losses++;
+            alert("you lost, dummy! The answer is " + countryName);
+            document.querySelector("#loss-counter").innerHTML = losses;
+          }
+
+    }
+    //new game
+    document.querySelector("#button").addEventListener("click", newGame);
